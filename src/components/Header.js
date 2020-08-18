bcf04c836f9a742f0c44a274639bd4fa55cf09d1
zelia-6385 <reshka6385@gmail.com>
2020-08-19 00:01:04 +0300

    make components

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://lh3.googleusercontent.com/proxy/d997NXHlxabLRYVeX6eUmFqHu3mLxAgTPX-RSWyBqhEzc7Ip24ad2N7M-p4JVOPT4SiE_Jzshr9TAow2D1_8Yb8KFXTsefZUxCA1hKA5R1skbGaCE_DD-Q"
+                alt="logo"
+            />
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528__340.jpg"
+                    alt="banner"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
