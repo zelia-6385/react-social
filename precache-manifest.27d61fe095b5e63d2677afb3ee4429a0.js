8dd9dd2abdcf38f0602ba01c70f5943a3c7573ea
zelia-6385 <reshka6385@gmail.com>
2020-09-16 19:46:08 +0300

    Updates

---
+++ precache-manifest.27d61fe095b5e63d2677afb3ee4429a0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "460c4e2f0d14ddaebcd1f886ba33a0c3",
+    "url": "/react-social/index.html"
+  },
+  {
+    "revision": "e1112a0f5c245025308a",
+    "url": "/react-social/static/css/3.ca6bc6ee.chunk.css"
+  },
+  {
+    "revision": "d2fab892aab091671a18",
+    "url": "/react-social/static/css/4.ed60209e.chunk.css"
+  },
+  {
+    "revision": "2cc75213f438f1a820d0",
+    "url": "/react-social/static/css/main.fb123577.chunk.css"
+  },
+  {
+    "revision": "d6bf026c8106f805b3f3",
+    "url": "/react-social/static/js/2.7f0e52ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-social/static/js/2.7f0e52ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1112a0f5c245025308a",
+    "url": "/react-social/static/js/3.8bdfa365.chunk.js"
+  },
+  {
+    "revision": "d2fab892aab091671a18",
+    "url": "/react-social/static/js/4.1e27759a.chunk.js"
+  },
+  {
+    "revision": "2cc75213f438f1a820d0",
+    "url": "/react-social/static/js/main.3f97f00f.chunk.js"
+  },
+  {
+    "revision": "6c0294cb6768a5a00118",
+    "url": "/react-social/static/js/runtime-main.c2d21fac.js"
+  },
+  {
+    "revision": "a68cb55946c8bb06186d649aa7c8ba77",
+    "url": "/react-social/static/media/Spinner-1s-200px.a68cb559.svg"
+  }
+]);
